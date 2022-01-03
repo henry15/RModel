fee08150649b597c7691df656edad660eff8b881
Henry <henrychettiar@hotmail.com>
2022-01-03 12:21:27 +0530

    Update Recommendation.R

--- Recommendation.R
+++ Recommendation.R
@@ -1,26 +1,18 @@
-library(jsonlite)
-library(sqldf)
-
-#* @get /
-function(input) {
-  toJSON(input)
-}
-
 ## Steps to host in local
-#library(plumber)
-#library(jsonlite)
+library(plumber)
+library(jsonlite)
 #r <- plumb("D:/ISM/structureValidatingProcedures/Task3_B.R")   -- give your folder path
 #r$run(port = 8000)
 
 ####-----Recommendation Engine -----
 
-#* @get  /analyse
+#* @get  /Recommendation
 function(input){
 # input the searchCategory id as per the requirement
 searchCategory= input  ##'133629'
 
 #read the data ---- Note: Alter the file path as per your location
-data<- read.csv("./CategoryNamesList.xlsx") 
+data<- read.csv("D:\\ISM\\structureValidatingProcedures\\Task3DataSet\\rub_dat.csv") 
 filter=subset(data, category_id==searchCategory) #filter the data as per input
 frame=data.frame(filter)
 customerIds=frame[1]   # frame1 index fetches the customerId column values
@@ -31,7 +23,7 @@ for(customerid in customerIds){
 }
 
 frequencyData=data.frame(filteredCategoryId)
-
+library(sqldf)
 # query above frequencyData variable to get the frequency count
 RecommendedCategories= sqldf(sprintf("select category_id,count(category_id) as frequency from frequencyData 
                               where category_id != '%s'
@@ -40,7 +32,7 @@ RecommendedCategories= sqldf(sprintf("select category_id,count(category_id) as f
 
 
 # Read the categoryName file and query through it to fetch corresponding names for recommendedCategoryIds
-CategoryNameData<- read.csv("./Strukturbaum_Namen.csv") 
+CategoryNameData<- read.csv("D:\\ISM\\structureValidatingProcedures\\Task3DataSet\\Strukturbaum_Namen.csv") 
 RecommendedCategoryName=sqldf(sprintf("select rc.frequency,category_de as GermanName, category_en as EnglishName 
                         from CategoryNameData cd
                         inner join RecommendedCategories rc on rc.category_id = cd.category_id
@@ -51,11 +43,8 @@ RecommendedCategoryName=sqldf(sprintf("select rc.frequency,category_de as German
 #Get the categoryName for input categoryId
 searchcategoryName= subset(CategoryNameData, category_id == searchCategory, select = category_de)
 
-print(paste('TOP 10 Recommendations for \'',searchcategoryName[1,],'\' are: '))
-print(as.data.frame(RecommendedCategoryName))
-#return 'test'
-toJSON(as.data.frame(RecommendedCategoryName))
-#toString("test")
-  #toString(as.data.frame(RecommendedCategoryName))
-#return(list(response = jsonlite::unbox("This is my text")))
+#print(paste('TOP 10 Recommendations for \'',searchcategoryName[1,],'\' are: '))
+#print(as.data.frame(RecommendedCategoryName))
+
+ return(toJSON(as.data.frame(RecommendedCategoryName)))
 }
