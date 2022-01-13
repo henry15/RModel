5b8f87e2679139da0e1379c4b0ca1403824d6a04
Henry <henrychettiar@hotmail.com>
2022-01-13 22:59:41 +0530

    Update Recommendation.R

--- Recommendation.R
+++ Recommendation.R
@@ -19,7 +19,7 @@ data<- read.csv("rub_dat_Minimised.csv")
 filter=subset(data, category_id==searchCategory) #filter the data as per input
 frame=data.frame(filter)
 customerIds=frame[1]   # frame1 index fetches the customerId column values
-
+print(nrow(customerIds))
 for(customerid in customerIds){
   #find categoryIds for each customerid and store in variable
   filteredCategoryId= subset(data, customer_id %in% customerid, select = category_id)
@@ -28,20 +28,20 @@ for(customerid in customerIds){
 frequencyData=data.frame(filteredCategoryId)
 library(sqldf)
 # query above frequencyData variable to get the frequency count
-RecommendedCategories= sqldf(sprintf("select category_id,count(category_id) as frequency from frequencyData 
+RecommendedCategories= sqldf(sprintf("select category_id,count(category_id) as frequency, cast(count(category_id) AS real)/%s as RelFreq from frequencyData 
                               where category_id != '%s'
                               Group By category_id 
-                              order by frequency desc Limit 25",searchCategory))
+                              order by frequency desc Limit 25",nrow(customerIds),searchCategory))
 
 
 # Read the categoryName file and query through it to fetch corresponding names for recommendedCategoryIds
-CategoryNameData<- read.csv("Strukturbaum_Namen.csv") 
-RecommendedCategoryName=sqldf(sprintf("select rc.frequency,category_de as GermanName, category_en as EnglishName 
+CategoryNameData <- read.csv("Strukturbaum_Namen.csv") 
+RecommendedCategoryName = sqldf(sprintf("select rc.RelFreq as RelativeFrequency, rc.frequency as Frequency, category_de as GermanName, category_en as EnglishName 
                         from CategoryNameData cd
                         inner join RecommendedCategories rc on rc.category_id = cd.category_id
                         and cd.category_id != '%s'
                         group by cd.category_id
-                        order by rc.frequency desc Limit 25",searchCategory))
+                        order by rc.RelFreq, rc.frequency  desc Limit 25",searchCategory))
 
 #Get the categoryName for input categoryId
 searchcategoryName= subset(CategoryNameData, category_id == searchCategory, select = category_de)
